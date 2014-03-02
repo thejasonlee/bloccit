f655e51d128c7fe04cd72f5f5b519886474ac8ad
Jason Weidner <jweidner@gmail.com>
2014-03-02 18:31:11 +1000

    First commit and README update

---
+++ REAME.md
@@ -0,0 +1,5 @@
+# Bloccit
+
+A Reddit-style application to demonstrate foundation programming and web development skills.
+
+Made by Jason Weidner with my mentor Joost Baaij at [Bloc](http://bloc.io)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloccit::Application.config.secret_key_base = 'd0d1b93daa68e23f4165a0bbe9e9321be1fa871fddce31d4f180c083b0bf9feefc5e54d4810b045c5ac04f97d3f92c6777aae802283ee9b4bca86b19ea4e84d1'
