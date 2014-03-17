3840b045e6251517354e33f4645aa0d817dfeeaa
Jason Weidner <jweidner@gmail.com>
2014-03-17 18:54:24 +1000

    Implemented Cancan and basic roles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,11 +9,14 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    authorize! :create, Post, message: "You need to be a member to create a new post."
   end
 
   def create
     @post = current_user.posts.build(post_params)
-
+    
+    authorize! :create, @post, message: "You need to be signed up to do that."
+    
     if @post.save
       flash[:notice] = "Post was saved."
       redirect_to @post
@@ -25,10 +28,12 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
+    authorize! :edit, @post, message: "You need to own the post to edit it."
   end
 
   def update
     @post = Post.find(params[:id])
+    authorize! :update, @post, message: "You need to own the post to edit it."
     if @post.update_attributes(post_params)
       flash[:notice] = "Post was updated."
       redirect_to @post
@@ -37,9 +42,11 @@ class PostsController < ApplicationController
       render :edit
     end
   end
+    
+    private
+      def post_params
+        params.require(:post).permit(:title, :body)
+      end
 end
 
-    private
-        def post_params
-          params.require(:post).permit(:title, :body)
-        end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :posts
+
+  before_create :set_member
+
+  ROLES = %w[member moderator admin]
+  def role?(base_role)
+    role.nil? ? false : ROLES.index(base_role.to_s) <= ROLES.index(role)
+  end
+
+  private
+
+  def set_member
+    self.role = 'member'
+  end
 end
