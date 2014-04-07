f6110aad8031349d908e27812e5de75a110f1da8
Jason Weidner <jweidner@gmail.com>
2014-04-07 15:34:41 +1000

    Scoped topics for privacy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
 
   def show
     @topic = Topic.find(params[:topic_id])
+    authorize @topic
     @post = Post.find(params[:id])
     @comments = @post.comments
     @comment = Comment.new
