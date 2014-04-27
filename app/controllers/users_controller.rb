cbb4d6fc86a5fd774a736ed1a9bf2b76b3452bf0
Jason Weidner <jweidner@gmail.com>
2014-04-27 10:59:12 +1000

    Created user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    authorize @user
+    @posts = @user.posts.visible_to(current_user)
   end
 
   def update
--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,7 @@ class Post < ActiveRecord::Base
   #mount_uploader :image, ImageUploader
 
   default_scope order('rank DESC')
+  scope :visible_to, ->(user) { user ? scoped : joins(:topic).where('topics.public' => true) }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
